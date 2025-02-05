3da702f4f9edc9ff71d3597eb5b3b8ea2008ddcf
ruben <tterubenator@gmai.com>
2025-02-05 17:01:04 +0100

    add Adaptadores

---
+++ app/src/main/java/com/example/odyssiaproject/adaptador/AdaptadorPaises.java
@@ -0,0 +1,74 @@
+package com.example.odyssiaproject.adaptador;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.odyssiaproject.R;
+import com.example.odyssiaproject.entidad.Pais;
+import com.example.odyssiaproject.negocio.negocioPaises;
+
+import java.util.List;
+
+public class AdaptadorPaises extends RecyclerView.Adapter<AdaptadorPaises.ViewHolder>{
+    private Pais p;
+    private List<Pais> listaPais;
+    public AdaptadorPaises(List<Pais> listaPaises) {
+        this.listaPais = listaPaises;
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imagenPais;
+
+        public ViewHolder(View v) {
+            super(v);
+            imagenPais = v.findViewById(R.id.cityImage);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_countries, parent, false);
+        AdaptadorPaises.ViewHolder viewHolder = new AdaptadorPaises.ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        negocioPaises nP = new negocioPaises();
+        p = listaPais.get(position);
+        if (nP.imagenPaises(p) == 1){
+            holder.imagenPais.setImageResource(R.drawable.listaspain);
+        } else if (nP.imagenPaises(p) == 2) {
+            holder.imagenPais.setImageResource(R.drawable.listaitaly);
+        }else if (nP.imagenPaises(p) == 3) {
+            holder.imagenPais.setImageResource(R.drawable.listafrance);
+        }else if (nP.imagenPaises(p) == 4) {
+            holder.imagenPais.setImageResource(R.drawable.listaswitzerland);
+        }else if (nP.imagenPaises(p) == 5) {
+            holder.imagenPais.setImageResource(R.drawable.listagreece);
+        }else if (nP.imagenPaises(p) == 6) {
+            holder.imagenPais.setImageResource(R.drawable.listaportugal);
+        }else if (nP.imagenPaises(p) == 7) {
+            holder.imagenPais.setImageResource(R.drawable.listabelgium);
+        }else if (nP.imagenPaises(p) == 8) {
+            holder.imagenPais.setImageResource(R.drawable.listanorway);
+        }else if (nP.imagenPaises(p) == 9) {
+            holder.imagenPais.setImageResource(R.drawable.listaengland);
+        }else if (nP.imagenPaises(p) == 10) {
+            holder.imagenPais.setImageResource(R.drawable.listaitaly);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPais.size();
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/odyssiaproject/singelton/ListaPromocionesSingelton.java
@@ -0,0 +1,54 @@
+package com.example.odyssiaproject.singelton;
+
+import android.util.Log;
+
+import com.example.odyssiaproject.entidad.Promociones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaPromocionesSingelton {
+    private static ListaPromocionesSingelton instance;
+    private List<Promociones> listaPromociones;
+    private int contador = 1;
+
+    private ListaPromocionesSingelton(){
+        super();
+    }
+
+    public static ListaPromocionesSingelton getInstance() {
+        if(instance == null){
+            instance = new ListaPromocionesSingelton();
+        }
+        return instance;
+    }
+
+    public Promociones getPromocionById(int id) {
+        for (Promociones p : listaPromociones) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+    public void inicializar(){
+        listaPromociones = new ArrayList<>();
+        Promociones promociones = new Promociones();
+        promociones.setId(contador++);
+        promociones.setNombre("Rebajas");
+
+        listaPromociones.add(promociones);
+
+        promociones = new Promociones();
+        promociones.setId(contador++);
+        promociones.setNombre("Pack");
+
+        listaPromociones.add(promociones);
+
+        Log.i("ListaPromocionesSingleton", "########" + promociones);
+    }
+
+    public List<Promociones> getListaPromociones() {
+        return listaPromociones;
+    }
+}
