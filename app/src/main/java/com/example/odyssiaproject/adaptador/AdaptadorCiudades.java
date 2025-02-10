6e91f5f6f615cb23c9a5b2823aa255d1435070f8
ruben <tterubenator@gmai.com>
2025-02-10 15:52:13 +0100

    add cambios

--- app/src/main/java/com/example/odyssiaproject/adaptador/AdaptadorCiudades.java
+++ app/src/main/java/com/example/odyssiaproject/adaptador/AdaptadorCiudades.java
@@ -1,5 +1,30 @@
 package com.example.odyssiaproject.adaptador;
 
-public class AdaptadorCiudades {
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.odyssiaproject.R;
+import com.example.odyssiaproject.entidad.Ciudad;
+import com.example.odyssiaproject.entidad.Pais;
+
+import java.util.List;
+
+public class AdaptadorCiudades {
+    private Ciudad c;
+    private List<Ciudad> listaCiudades;
+
+    public AdaptadorCiudades(List<Ciudad> listaCiudades) {
+        this.listaCiudades = listaCiudades;
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imagenPais;
+
+        public ViewHolder(View v) {
+            super(v);
+            imagenPais = v.findViewById(R.id.imageCountries);
+        }
+    }
 }
